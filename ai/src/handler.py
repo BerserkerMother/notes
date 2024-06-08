2d4428594b367cddc56dedf9f594d702024c82ef
Kave Bahraman <sadegh.b218@gmail.com>
2024-06-08 19:17:15 +0330

    Adds db add remove get all

--- ai/src/handler.py
+++ ai/src/handler.py
@@ -5,6 +5,6 @@ app = Flask("ai_engine_api")
 
 @app.route("/")
 def home():
-    return "embedding engine is ready!"
+    return "embedding engine is ready! is this magic?"
 
 app.run(debug=True, host="0.0.0.0")
--- ai/src/vector_db.py
+++ ai/src/vector_db.py
@@ -1,5 +1,6 @@
 import logging
 import os
+from typing import List
 
 from dotenv import load_dotenv
 from pymongo.mongo_client import MongoClient
@@ -12,18 +13,44 @@ formatter = logging.Formatter(
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("db")
 
+class VectorDB:
+    def __init__(self, conn_string: str):
+        self.client = self._get_client(conn_string)
+        self.embedding_db = self.client.note.embeddings
 
-def get_client(conn_stirng: str):
-    try:
-        client = MongoClient(conn_stirng)
-        client.admin.command("ping")
-        logger.info("connection to db established")
-        return client
-    except Exception as e:
-        logger.info(f"db doesn't response with error: {e}")
+    def add_emb(self, _id: int, embeddings: List[float]):
+        self.embedding_db.insert_one(dict(_id=_id, vec=embeddings))
+
+    def remove_emb(self, _id: int):
+        self.embedding_db.delete_one({"_id": _id})
+
+    def get_embds(self):
+        ids, embds = [], []
+        for item in self.embedding_db.find():
+            ids.append(item["_id"])
+            embds.append(item["vec"])
+        return ids, embds
+
+    def _get_client(self, conn_stirng: str):
+        try:
+            client = MongoClient(conn_stirng)
+            client.admin.command("ping")
+            logger.info("connection to db established")
+            return client
+        except Exception as e:
+            logger.info(f"db doesn't response with error: {e}")
 
 
 if __name__ == "__main__":
-    logger.info(os.environ.get("VECTOR_DB_CONN_STRING"))
-    client = get_client(os.environ.get("VECTOR_DB_CONN_STRING"))
-    logging.info(client)
+    conn_string = os.environ.get("VECTOR_DB_CONN_STRING")
+    vec = [8] * 100
+    db = VectorDB(conn_string)
+    db.add_emb(0, vec)
+    db.add_emb(1, vec)
+    db.add_emb(2, vec)
+    get_all = db.get_embds()
+    logger.info(get_all)
+
+    for item in db.client.note.embeddings.find():
+        logger.info(item)
+        db.remove_emb(item["_id"])
