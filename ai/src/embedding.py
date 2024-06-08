a25a41872f23607015ea66046925cad05cd7e3fa
Kave Bahraman <sadegh.b218@gmail.com>
2024-06-08 22:56:04 +0330

    Adds sentence transformer text embeddings

--- ai/src/embedding.py
+++ ai/src/embedding.py
@@ -1,11 +1,25 @@
+import logging
 from typing import List
 
 import numpy as np
+from sentence_transformers import SentenceTransformer
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("embeddings")
+
+# Load the model
+model = SentenceTransformer("obrizum/all-MiniLM-L6-v2")
+
+def embed(sentences: List[str]) -> List[List[float]]:
+    embds = model.encode(sentences)[0]
+    logger.info(embds)
+    return embds
 
 
-def cosine_sim(query: List[float], source: List[List[float]]):
+def cosine_sim(query: List[List[float]], source: List[List[float]]):
     query_arr = np.array(query).reshape(-1, 1)  # reshape for dot product
     source_arr = np.array(source)
+    logger.info(source_arr.shape)
 
     # normalize
     source_arr = source_arr / np.linalg.norm(source_arr, axis=1, keepdims=True)
